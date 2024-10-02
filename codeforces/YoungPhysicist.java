4047afa0023ed41eaa001f1626abb280fe290fb1
taherallaa <ta7ermo2002@gmail.com>
2024-10-02 19:40:56 +0300

    Solve Young Physicist  problem #69A

---
+++ codeforces/YoungPhysicist.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * YoungPhysicist #69A
+ */
+public class YoungPhysicist {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int LINE = scan.nextInt();
+    int result = 0;
+
+    int[][] force = new int[LINE][3];
+
+    int[] idelBody = { 0, 0, 0 };
+
+    for (int i = 0; i < LINE; i++) {
+      for (int j = 0; j < 3; j++) {
+        idelBody[j] += scan.nextInt();
+      }
+    }
+
+    scan.close();
+
+    for (int i : idelBody) {
+      if (i != 0) {
+        System.out.println("NO");
+        return;
+      }
+    }
+    System.out.println("YES");
+  }
+}
