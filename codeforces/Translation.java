36b81bf1920c9b0810bae8ea769ea3cf0d37aa01
taherallaa <ta7ermo2002@gmail.com>
2024-10-06 15:09:32 +0300

    Solve Translate  problem #41A

---
+++ codeforces/Translation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Translation #41A
+ */
+public class Translation {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String word = scan.next();
+    String targetWord = scan.next();
+    StringBuilder translationWord = new StringBuilder();
+
+    for (int i = word.length() - 1; i >= 0; i--) {
+      translationWord.append(word.charAt(i));
+    }
+
+    if (translationWord.toString().equals(targetWord)) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
