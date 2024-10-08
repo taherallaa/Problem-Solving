7f434edcffea0f5b86bf5152bcb33e909fbf07b9
taherallaa <ta7ermo2002@gmail.com>
2024-10-08 22:18:53 +0300

    Solve chat room  problem #58A

---
+++ codeforces/ChatRoom.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * ChatRoom #58A
+ */
+public class ChatRoom {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input = scan.next();
+
+    String target = "hello";
+    int pointer = 0;
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == target.charAt(pointer))
+        pointer++;
+      if (pointer == target.length()) {
+        System.out.println("YES");
+        return;
+      }
+    }
+
+    System.out.println("NO");
+  }
+
+}
