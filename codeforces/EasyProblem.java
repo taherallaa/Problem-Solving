98038c058dc5c497d082b26ff679da9c2c627079
taherallaa <ta7ermo2002@gmail.com>
2024-10-08 21:25:41 +0300

    Solve queue school year  problem #1030A

---
+++ codeforces/EasyProblem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * EasyProblem #1030A
+ */
+public class EasyProblem {
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int numAskedPeopole = scan.nextInt();
+
+    for (int i = 0; i < numAskedPeopole; i++) {
+      if (scan.nextInt() == 1) {
+        System.out.println("HARD");
+        return;
+      }
+    }
+    System.out.println("EASY");
+  }
+
+}
