0c01bae863b2de5e34f892742a319715930fbdde
taherallaa <ta7ermo2002@gmail.com>
2024-10-10 19:41:05 +0300

    Solve even odds problem #318A

---
+++ codeforces/EvenOdds.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import static java.lang.Math.ceil;
+
+/**
+ * EvenOdds #318A
+ */
+public class EvenOdds {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long n = sc.nextLong();
+    long k = sc.nextLong();
+    sc.close();
+
+    long oddCount = (n + 1) / 2;
+
+    if (k <= oddCount) {
+      System.out.println(2 * k - 1);
+    } else {
+      System.out.println(2 * (k - oddCount));
+    }
+  }
+}
