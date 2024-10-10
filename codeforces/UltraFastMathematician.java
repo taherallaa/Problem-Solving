2f3e958e8b83f7725e66cb0e7bad2f697f3427c3
taherallaa <ta7ermo2002@gmail.com>
2024-10-10 21:12:50 +0300

    Solve ultra fast mathematician problem #61A

---
+++ codeforces/UltraFastMathematician.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * UltraFastMathematician #61A
+ */
+public class UltraFastMathematician {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String fNum = scan.next();
+    String sNum = scan.next();
+
+    StringBuilder result = new StringBuilder();
+
+    for (int i = 0; i < fNum.length(); i++) {
+      if (fNum.charAt(i) == sNum.charAt(i)) {
+        result.append('0');
+      } else {
+        result.append('1');
+      }
+    }
+    System.out.println(result);
+  }
+}
