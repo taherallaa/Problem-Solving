a403211a50d8024edefc53c2525ec585663b97c5
taherallaa <ta7ermo2002@gmail.com>
2024-10-10 09:59:37 +0300

    Solve presents problem #136A

---
+++ codeforces/Presents.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Presents
+ */
+public class Presents {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int friendNumber = scan.nextInt();
+    int[] friends = new int[friendNumber];
+
+    for (int i = 0; i < friendNumber; i++) {
+      friends[scan.nextInt() - 1] = i + 1;
+    }
+
+    for (int i = 0; i < friendNumber; i++) {
+      System.out.print(friends[i] + " ");
+    }
+
+    scan.close();
+  }
+}
