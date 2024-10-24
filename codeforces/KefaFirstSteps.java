370e7c46fb416a358db9122964e6e85722c2e0c7
taherallaa <ta7ermo2002@gmail.com>
2024-10-24 10:14:41 +0300

    Solve kefa first step #580A,  problem in 2024-10-24

---
+++ codeforces/KefaFirstSteps.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * KefaFirstSteps #589A
+ */
+public class KefaFirstSteps {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int days = scan.nextInt();
+
+    int counter = 1;
+    int maxSegmant = 1;
+
+    int prevDay = scan.nextInt();
+
+    for (int i = 1; i < days; i++) {
+      int curDay = scan.nextInt();
+
+      if (curDay >= prevDay) {
+        counter++;
+        maxSegmant = Math.max(maxSegmant, counter);
+      } else {
+        counter = 1;
+      }
+
+      prevDay = curDay;
+    }
+
+    System.out.println(maxSegmant);
+
+    scan.close();
+  }
+}
